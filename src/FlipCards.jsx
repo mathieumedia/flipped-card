95d58c9a9abc05b77ca34d57351bce3111cf3163
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-05-31 01:33:33 -0400

    Created layouts

---
+++ src/FlipCards.jsx
@@ -0,0 +1,29 @@
+
+export function CardContainer (props){
+
+}
+
+export function FlipCard(props){
+
+}
+
+function HoverFlip (props) {
+
+}
+
+function ClickFlip (props) {
+
+}
+
+export function Front (props) {
+
+}
+
+export function Back (props) {
+
+
+}
+
+export function VerticalBack (props) {
+
+}
